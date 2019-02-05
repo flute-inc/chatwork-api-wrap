20b85f247e3784269b4a87b1948031cf2ce97f01
holyshared <holy.shared.design@gmail.com>
2019-02-05 16:21:00 +0900

    add token header function

---
+++ src/ts/request.ts
@@ -0,0 +1,7 @@
+import request from "superagent";
+import { AUTHORIZATION_TOKEN, CHATWORK_TOKEN } from "./constants";
+
+export const withToken = (apiToken: string) => (request: request.Request) : request.Request => {
+    return request.set(CHATWORK_TOKEN, apiToken)
+        .set(AUTHORIZATION_TOKEN, `Bearer ${apiToken}`);
+};
